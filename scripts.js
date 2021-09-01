933bdba1f93ffe671c4c1ed96a3402a5009e5298
Matt Eichsteadt <matteichsteadt@Matts-MacBook-Air.local>
2021-09-01 11:19:53 -0700

    add form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
+
+  $("#contact-form").on('submit', function(event) {
+    event.preventDefault();
+    var email = $('form input[name=email]').val();
+    var name = $('form input[name=name]').val();
+    var storeName = $('form input[name=storeName]').val();
+    var numStores = $('form input[name=numStores]').val();
+    var data = {
+      "lead": {
+        "email": email,
+        "name": name,
+        "store_name": storeName,
+        "num_stores": numStores,
+      }
+    }
+    $("input").val(null);
+    this.reset();
+    $("#contact-form").replaceWith("<p>Thank you for reaching out! We'll be contacting you very shortly!</p>");
+    postLead(data);
+  });
+
+  function postLead(data) {
+    $.post("http://localhost:3000/leads", data)
+  }
 });
